e49f11a4902cc6c8ee0b42f96b24d6984a3caf4a
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-06-17 20:32:45 +0200

    Revert "flights to visible change when logged out"
    This reverts commit 40a69dba05b27322e830d65f30c0ca82c6b0e117.

--- dist/flight-management-system.ui/browser/chunk-LPXM25NO.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-5YEZSZ6G.js";import"./chunk-MUP74BBE.js";export{a as FlightModule};
---
+++ dist/flight-management-system.ui/browser/chunk-PK24736M.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-6DIKQ5O7.js";import"./chunk-MUP74BBE.js";export{a as FlightModule};
