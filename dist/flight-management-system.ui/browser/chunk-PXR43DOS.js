6538e649e7663b926d3167c0509c154c0e8fe8fd
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-06-18 18:12:37 +0200

    added a bit of responsiveness etc. and fixes

---
+++ dist/flight-management-system.ui/browser/chunk-PXR43DOS.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-UKGET6AS.js";import"./chunk-ZW7RAY6D.js";export{a as FlightModule};
--- dist/flight-management-system.ui/browser/chunk-STILJ4L2.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-UMPMVNXD.js";import"./chunk-ZW7RAY6D.js";export{a as FlightModule};
